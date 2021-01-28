15afa91fe9820aea24b66c57d091e7fe12a4606e
Eric Soderberg <eric.soderberg@hpe.com>
2021-01-28 09:15:00 -0800

    Fixed Figma and GitHub icon issue

--- src/LinkIcon.js
+++ src/LinkIcon.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Figma, Github, Link } from 'grommet-icons';
 
 const LinkIcon = ({ url }) => {
-  if (url.includes('figma.com')) return <Github />;
-  if (url.includes('github.com')) return <Figma color="plain" />;
+  if (url.includes('figma.com')) return <Figma color="plain" />;
+  if (url.includes('github.com')) return <Github />;
   return <Link />;
 };
 
